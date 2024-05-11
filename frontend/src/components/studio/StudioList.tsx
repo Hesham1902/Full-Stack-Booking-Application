ea1d05da0ef9acd57ed1151ecce3fde9a8bfbaf2
Hesham1902 <100668426+Hesham1902@users.noreply.github.com>
2024-05-11 16:30:01 +0300

    last frontend update

--- frontend/src/components/studio/StudioList.tsx
+++ frontend/src/components/studio/StudioList.tsx
@@ -6,20 +6,26 @@ import { studio } from "../../types";
 const StudioList = ({ studios }: { studios: studio[] }) => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 place-items-center lg:place-items-start gap-8 mt-8">
-      {studios.map((studio, index) => (
-        <Fragment key={index}>
-          <HomeStudioCard
-            img={studio_img}
-            title={studio.name}
-            price_per_day={studio.price_per_day}
-            start_time={studio.start_time}
-            end_time={studio.end_time}
-            location={studio.address}
-            path={String(studio.id)}
-            owner_id={studio.owner_id}
-          />
-        </Fragment>
-      ))}
+      {studios.length !== 0 ? (
+        studios.map((studio, index) => (
+          <Fragment key={index}>
+            <HomeStudioCard
+              img={studio_img}
+              title={studio.name}
+              price_per_day={studio.price_per_day}
+              start_time={studio.start_time}
+              end_time={studio.end_time}
+              location={studio.address}
+              path={String(studio.id)}
+              owner_id={studio.owner_id}
+            />
+          </Fragment>
+        ))
+      ) : (
+        <h2 className="text-xl">
+          Sorry, There is no studios available right now.
+        </h2>
+      )}
     </div>
   );
 };
